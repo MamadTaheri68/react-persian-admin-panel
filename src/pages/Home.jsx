6f0880daec3d1a281365d78a40096d25b2930de3
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-17 03:11:20 +0430

    add portfolio page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,34 +3,24 @@ import React from 'react';
 const Home = () => {
     return (
         <div className='main-wrapper'>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
-            <h1>Home Home Home Home Home Home</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
+            <h1>داشبوردداشبوردداشبوردداشبوردداشبورد</h1>
         </div>
     );
 };
--- src/pages/WebRoutes.jsx
+++ src/pages/WebRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import Portfolio from './Portfolio';
 
 const WebRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
     );
 };
