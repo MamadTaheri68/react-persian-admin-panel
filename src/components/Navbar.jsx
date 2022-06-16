8040d86951901ba5c68016d92617abc23318ac33
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-17 00:38:17 +0430

    upgrade Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,36 @@ import React from "react";
 import "./Navbar.css";
 import * as FaIcons from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar((showSidebar) => !showSidebar);
+  };
   return (
     <>
       <div className="navbar">
         <div className="nav-right">
           <Link to="#" className="menu-bars">
-            <FaIcons.FaBars />
+            <FaIcons.FaBars onClick={toggleSidebar} />
           </Link>
           <h1>ادمین پنل ری اکت ورژن یک</h1>
         </div>
         <div className="nav-left">
-            <h1>آیکن های سمت چپ</h1>
+          <h1>آیکن های سمت چپ</h1>
         </div>
       </div>
-      <nav className="nav-menu"></nav>
+      <nav className={showSidebar ? "nav-menu active" : "nav-menu"}>
+        <ul>
+          <li>item #1</li>
+          <li>item #2</li>
+          <li>item #3</li>
+          <li>item #4</li>
+          <li>item #5</li>
+        </ul>
+      </nav>
     </>
   );
 };
